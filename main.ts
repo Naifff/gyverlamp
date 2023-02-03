5af8077218066b38d3431523477b90a8fed26ab8
Naif <knifffe@yandex.ru>
2023-02-03 16:28:48 +0300

    sonar

--- main.ts
+++ main.ts
@@ -1,15 +1,32 @@
-input.onSound(DetectedSound.Loud, function () {
-    while (loop) {
-    	
+input.onButtonPressed(Button.A, function () {
+    if (loop) {
+        loop = false
+    } else {
+        loop = true
     }
 })
+let dist = 0
 let loop = false
 loop = false
 let strip = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
 basic.forever(function () {
-    for (let i = 0; i <= 360; i++) {
-        for (let j = 0; j <= 7; j++) {
-            strip.setPixelColor(j, neopixel.hsl(i / (j * 45), 100, 50))
+    dist = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    basic.pause(1000)
+    if (loop) {
+        for (let i = 0; i <= 360; i++) {
+            for (let j = 0; j <= 7; j++) {
+                strip.setPixelColor(j, neopixel.hsl(i / (j * 45), 100, 50))
+            }
+            strip.show()
         }
+    }
+    if (loop) {
+        basic.showNumber(1)
+    } else {
+        basic.showNumber(0)
     }
 })
